8f2490002b31873fc787d4fe94ed157dc8fd144f
Ander1977 <kopa.andrej@gmail.com>
2020-05-27 19:32:14 +0300

    Changed maven.yml

--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -9,11 +9,6 @@ class ConditionerTest {
     @Test
     void increaseCurrentTemperature() {
         Conditioner conditioner = new Conditioner();
-        assertEquals(expected: 21, );
-        int setCurrentTemperature = 20;
-        int expected = 21;
 
-
-        assertEquals(expected);
     }
 }
